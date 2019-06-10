06a06a844ef8aac5da487b4bfd197248378a60f4
huangchenyao <hcy_96@outlook.com>
2019-06-10 11:23:27 +0800

    javadoc

---
+++ src/main/java/com/abc/jdk9/Javadoc.java
@@ -0,0 +1,18 @@
+package com.abc.jdk9;
+
+/**
+ * @author Clearlove
+ * @version 4.396
+ */
+public class Javadoc {
+    /**
+     * function1
+     * <p>Hello world</p>
+     * @param param1 param 1
+     * @param param2 param 2
+     * @return return 0
+     */
+    public int function1(int param1, long param2) {
+        return 0;
+    }
+}
